3fe6e7f5323bebac4ae1b27ec4862bfb8840a6c2
akhilpanchal <akhilpanchal@gmail.com>
2025-05-03 12:30:29 -0700

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import CountdownTimer from "./components/Countdown";
+import CountdownTimer from "./Countdown";
 
 export default async function LandingPage() {
 
   return (
-    <main className="p-6 flex flex-col items-center justify-center min-h-screen">
+    <main className="flex flex-col items-center justify-center min-h-screen">
 
         <CountdownTimer />
         {/* <a href="/akhil" className="text-blue-500 hover:underline">
